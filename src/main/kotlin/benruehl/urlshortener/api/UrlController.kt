562ac84d16f1591a8061bfb92dd89287ec63116e
Benjamin Rühl <dev.ruehl@gmail.com>
2023-12-03 14:20:56 +0100

    feat: add endpoint for redirecting to original urls
    Implement endpoint that takes a URL's short ID, looks it up in the database, and returns the respective original URL with a redirect status code.

--- src/main/kotlin/benruehl/urlshortener/api/UrlController.kt
+++ src/main/kotlin/benruehl/urlshortener/api/UrlController.kt
@@ -2,6 +2,7 @@ package benruehl.urlshortener.api
 
 import benruehl.urlshortener.domain.repositories.UrlRepository
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 @RequestMapping("/u")
@@ -15,8 +16,9 @@ class UrlController(
         return UrlResponse(shortId = savedUrl.shortId)
     }
 
-    @GetMapping
-    fun redirectToOriginal(): String {
-        return "Hello"
+    @GetMapping("/{shortId}")
+    fun redirectToOriginal(@PathVariable shortId: String): RedirectView {
+        val savedUrl = repository.findByShortId(shortId)
+        return RedirectView(savedUrl.originalUrl)
     }
 }
