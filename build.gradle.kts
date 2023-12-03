688a07a8c5c6aaa02f944ee78e09a0db089d9f8a
Benjamin Rühl <dev.ruehl@gmail.com>
2023-12-03 20:20:13 +0100

    feat: add actuator and swagger-ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.h2database:h2")
